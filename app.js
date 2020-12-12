a0bcff973b40a1ec0ed8aed9fbe27d0cede75b8d
Riya Gupta git config --global user.email riyag0841@gmail.com <riyaaaaaaaa8@gmail.com>
2020-12-12 12:40:46 +0530

    Added the register page

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ app.get("/", function(req,res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+app.get("/speakers", function(req, res) {
+    res.sendFile(__dirname + "/speakers.html");
+})
+
+app.get("/schedule", function(req, res) {
+    res.sendFile(__dirname + "/schedule.html");
+})
+
+app.get("/register", function(req, res) {
+    res.sendFile(__dirname + "/register.html");
 })
 
 app.listen(3000, function(req, res) {
